5c3d957a8a9b99ab6127b7cb9f920b48c5ae2524
muhammad259 <malik12341414@gmail..com>
2025-02-12 09:46:46 +0500

    13

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,14 @@
-export default function RootLayout({
+export default function DashboardLayout({
     children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return <div className="h-screen flex">
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+    <div className="h-screen flex">
         {/* LEFT*/}
         <div className="w-[14%] md:[16%] bg-red-200">l</div>
         {/* right*/}
         <div className="w-[86%] md:[84%] bg-blue-200">r</div>
     </div>
-  }
+    );
+  };
